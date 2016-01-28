c380726c00b2b80a1990793813d73818bafdbdd0
Victor Boivie <victor@boivie.com>
2016-01-28 22:32:30 +0100

    Support for command line flags (and config file)

--- main.go
+++ main.go
@@ -1,17 +1,68 @@
 package main;
 import (
 	"net/http"
-	"github.com/boivie/undergang/app"
+	ug "github.com/boivie/undergang/app"
+	"github.com/codegangsta/cli"
 	"log"
+	"os"
+	"encoding/json"
+	"io/ioutil"
 )
 
-func main() {
-	app.Init("http://localhost:8001/_undergang/pathinfo", "", "")
 
-	log.Println("Accepting requests")
-	http.HandleFunc("/", app.Forward)
-	err := http.ListenAndServe(":8002", nil)
-	if err != nil {
-		panic(err)
+func main() {
+	app := cli.NewApp()
+	app.Name = "undergang"
+	app.Flags = []cli.Flag {
+		cli.StringFlag{
+			Name: "listen",
+			Value: ":8002",
+			Usage: "Listening address (ip and port)",
+		},
+		cli.StringFlag{
+			Name: "pathinfo",
+			Usage: "URL for pathinfo service",
+		},
+		cli.StringFlag{
+			Name: "access",
+			Usage: "URL for access service",
+		},
+		cli.StringFlag{
+			Name: "sshproxy",
+			Usage: "Optional utility for proxying SSH connections",
+		},
+		cli.StringFlag{
+			Name: "config",
+			Usage: "Configuration file",
+		},
+
 	}
+	app.Action = func(c *cli.Context) {
+		ug.Init(c.String("pathinfo"), c.String("access"), c.String("sshproxy"))
+
+		if c.String("config") != "" {
+			var config struct {
+				paths []ug.PathInfo
+			}
+			buf, err := ioutil.ReadFile(c.String("config"))
+			if err != nil {
+				panic(err)
+			}
+			if err = json.Unmarshal(buf, config); err != nil {
+				panic(err)
+			}
+			for _, path := range config.paths {
+				ug.AddPath(path)
+			}
+		}
+
+		log.Printf("Accepting requests on %s\n", c.String("listen"))
+		http.HandleFunc("/", ug.Forward)
+		err := http.ListenAndServe(c.String("listen"), nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	app.Run(os.Args)
 }
