abb1699cfa90316c0d8240371ee5537877e805ca
Victor Boivie <victor@boivie.com>
2016-02-03 07:56:19 +0100

    Added health check endpoint

--- app/init.go
+++ app/init.go
@@ -3,6 +3,7 @@ import (
 	"net/http"
 	"runtime"
 	"log"
+	"io"
 )
 
 var externalLookupUrl string
@@ -12,7 +13,10 @@ func dumpHandler(w http.ResponseWriter, req *http.Request) {
 	buf := make([]byte, 1 << 20)
 	runtime.Stack(buf, true)
 	log.Printf("=== received SIGQUIT ===\n*** goroutine dump...\n%s\n*** end\n", buf)
-	return
+}
+
+func healthHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "OK\n")
 }
 
 func Init(externalPathLookupUrl_ string, accessLookupUrl string, proxyCommand_ string) {
@@ -22,5 +26,6 @@ func Init(externalPathLookupUrl_ string, accessLookupUrl string, proxyCommand_ s
 	go backendManager()
 
 	http.HandleFunc("/__ug__dump", dumpHandler)
+	http.HandleFunc("/__ug__health", healthHandler)
 	http.HandleFunc("/", Forward)
 }
